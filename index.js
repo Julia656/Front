ea587d435f54f5d6cbdcd0737296d42e035ab420
Julia656 <ovcharenkojulia05@gmail.com>
2025-01-08 16:59:44 +0200

    ДЗ 6. ДЗ 4.2. Перевірка числа

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-let userName = prompt("Введіть ваше ім'я:");
-if (userName) {
-    console.log(`Привіт, ${userName}!`);
-    alert(`Привіт, ${userName}!`);
-} else {
-    console.log("Ви не ввели ім'я.");
-    alert("Ви не ввели ім'я.");
+function checkNumber(num) {
+    
+    num = num.trim();
+
+    
+    if (!/^[-]?\d{3}$/.test(num)) {
+        return "Помилка: введіть коректне тризначне число!";
+    }
+
+  
+    let digits = num.replace('-', '').split('');
+
+
+    const allSame = digits.every(digit => digit === digits[0]);
+    if (allSame) {
+        return "Усі цифри однакові.";
+    }
+
+   
+    const hasDuplicates = new Set(digits).size !== digits.length;
+    if (hasDuplicates) {
+        return "Є однакові цифри.";
+    }
+
+    return "Цифри всі різні.";
 }
 
 
-
-
-
-
-
-
-
-let userName = prompt("Введіть ваше ім'я:");
-if (userName && userName.trim() !== "") {
-    alert(`Hello, ${userName}! How are you?`);
-} else {
-    alert("Помилка: ім'я не введено. Використовую [noname].");
-    alert("Hello, [noname]! How are you?");
-}
+console.log(checkNumber('   123  '));  
+console.log(checkNumber('000456'));   
+console.log(checkNumber('-0621'));     
+console.log(checkNumber(' 3.2 '));    
